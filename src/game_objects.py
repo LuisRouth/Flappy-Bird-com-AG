f34ed340e0faa4a6c0d2ed3b71609a035af7b1a5
MarvinCristhian07 <marvincristhian07.contato@gmail.com>
2025-12-09 11:36:45 -0300

    Criando objeto Pipe (Cano)
    Criando objeto Pipe (Cano) e seus métodos.

--- src/game_objects.py
+++ src/game_objects.py
@@ -32,3 +32,34 @@ class Bird:
             
     def draw(self, win):
         pygame.draw.rect(win, (255, 0, 0), self.rect)
+        
+class Pipe:
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.top = 0
+        self.bottom = 0
+        self.passed = False
+        self.set_height()
+        
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - DIST_CANOS
+        self.bottom = self.height
+        
+        self.top_rect = pygame.Rect(self.x, 0, 70, self.height)
+        self.bottom_rect = pygame.Rect(self.x, self.height + DIST_CANOS, 70, ALTURA_TELA)
+        
+    def move(self):
+        self.x -= VEL_CANOS
+        self.top_rect.x = self.x
+        self.bottom_rect.x = self.x
+        
+    def collide(self, bird):
+        if bird.rect.colliderect(self.top_rect) or bird.rect.colliderect(self.bottom_rect):
+            return True
+        return False
+    
+    def draw(self, win):
+        pygame.draw.rect(win, (0, 255, 0), self.top_rect)
+        pygame.draw.rect(win, (0, 255, 0), self.bottom_rect)
